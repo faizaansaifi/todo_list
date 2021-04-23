f6f0ecf377b3268c5076520700069b1467124858
Faizaan Saifi <faizaan.saifi@tothenew.com>
2021-04-24 01:27:50 +0530

    master: First VueJs project

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import AddTask from '../views/AddTask.vue'
+import EditTask from '../views/EditTask.vue'
 
 const routes = [
   {
@@ -8,13 +10,16 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/add',
+    name: 'AddTask',
+    component: AddTask
+  },
+  {
+    path: '/projects/:id',
+    name: 'EditTask',
+    component: EditTask,
+    props: true
+  },
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="tasks.length">
+      <div v-for="task in tasks" :key="task.id">
+      <SingleTask :task="task" @delete="handleDelete" @complete="handleComplete" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleTask from '../components/SingleTask.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { SingleTask },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:8085/tasks')
+      .then(res => res.json())
+      .then(data => this.tasks = data)
+      .catch(err => console.log(err))
+  },
+  methods: {
+    handleDelete(id) {
+      this.tasks = this.tasks.filter(task => {
+        return task.id !== id
+      })
+    },
+    handleComplete(id) {
+      let p = this.tasks.find(task => {
+        return task.id === id
+      })
+      p.complete = !p.complete 
+      // console.log(p)
+    }
   }
 }
 </script>
